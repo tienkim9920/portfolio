3ce1e064a44e62f0334b511cce4306c1fb8e9bac
Tien Kim <tienkim9920@gmail.com>
2023-01-14 21:12:19 +0700

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () => {
+    console.log('listening on *: ' + PORT);
+});
